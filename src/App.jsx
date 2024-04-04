9f7229448136cbf55b4f45556bf7b79ffbeb9fe2
kuldeep-01 <kuldeepsinghdlw10072002@gmail.com>
2024-04-04 10:56:07 +0530

    desiging of home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
+import HomePage from "./pages/HomePage";
+
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
     <Routes>
+      <Route path="/home" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
     </Routes>
