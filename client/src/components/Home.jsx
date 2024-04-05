0a3c2f1b22c81d584014fb29f31a97cd81cdc497
kuldeep-01 <kuldeepsinghdlw10072002@gmail.com>
2024-04-05 21:52:20 +0530

    further modification in ui

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,9 @@
 function Home() {
   return (
     <>
-      <div class="container mx-auto flex items-center h-screen">
+      <div class="container mx-auto flex items-center h-screen bg-neutral-200">
 
-        <div class="w-1/2">
+        <div class="w-1/2 m-10">
 
           <h1 class="text-4xl font-bold text-black mb-4">Welcome to Our Website</h1>
           <p class="text-black mb-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
@@ -12,12 +12,14 @@ function Home() {
             <a href="/signup" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Sign Up</a>
           </div>
         </div>
-        <div class="w-1/2">
+        
+        <div class="w-1/2 m-10 shadow-2xl shadow-black rounded-lg">
           <img src="https://wallpapers.com/images/hd/dark-man-standing-idawk015ad468bbi.webp" alt="flower.jpg" class="mx-auto round rounded-md" />
         </div>
+
       </div>
 
-      <div class="bg-white bg-fixed" style={{ "background-image": "url('https://wallpapers.com/images/hd/dark-man-standing-idawk015ad468bbi.webp')" }}>
+      <div class="bg-white bg-fixed" style={{ "background-image": "url('https://wallpapers.com/images/hd/alone-minimalist-4k-rcdbzrzkz7fcljkq.webp')" }}>
 
         <div class="relative isolate px-6 pt-14 lg:px-8">
           <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
@@ -41,10 +43,91 @@ function Home() {
             </div>
           </div>
           <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
-            <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style={{ "clip-path": "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" }}></div>
+            {/* <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style={{ "clip-path": "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" }}></div> */}
           </div>
         </div>
       </div>
+
+{/* ###### some content */}
+      <div class="bg-neutral-200 py-24 sm:py-32">
+      <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div class="mx-auto max-w-2xl lg:mx-0">
+          <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
+          <p class="mt-2 text-lg leading-8 text-gray-600">Learn how to improve your work by an expert's advice.</p>
+        </div>
+        <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-black pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          <article class="flex max-w-xl flex-col items-start justify-between">
+        <div class="flex items-center gap-x-4 text-xs">
+          <time datetime="2020-03-16" class="text-gray-500">Mar 10, 2024</time>
+          <a href="#" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Technical</a>
+        </div>
+        <div class="group relative">
+          <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+            <a href="#">
+              <span class="absolute inset-0"></span>
+              Keep the tech up-to-date
+            </a>
+          </h3>
+          <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">It's a nice a platform but since similar platform already exist so make sure to improve the platform to make it stand out.</p>
+        </div>
+        <div class="relative mt-8 flex items-center gap-x-4">
+          <img src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="h-10 w-10 rounded-full bg-gray-50"/>
+          <div class="text-sm leading-6">
+            <p class="font-semibold text-gray-900">
+              <a href="#">
+                <span class="absolute inset-0"></span>
+                Mahatma Gandhi
+              </a>
+            </p>
+            <p class="text-gray-600">Lawyer / Activist</p>
+          </div>
+        </div>
+          </article>
+        </div>
+      </div>
+      </div>
+
+{/* ####### team member */}
+      <div class="bg-white py-24 sm:py-32">
+      <div class="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
+        <div class="max-w-2xl">
+          <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet our team</h2>
+          <p class="mt-6 text-lg leading-8 text-gray-600">It's a project by 3rd year students of CSEAI which allow the user to interact others. Try it and experience our cutting edge platform.</p>
+        </div>
+      <ul role="list" class="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
+      <li>
+        <div class="flex items-center gap-x-6">
+          <img class="h-16 w-16 rounded-full" src="https://images.unsplash.com/photo-1628260412297-a3377e45006f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2FydG9vbiUyMGZhY2V8ZW58MHx8MHx8fDA%3D" alt=""/>
+          <div>
+            <h3 class="text-base font-semibold leading-7 tracking-tight text-gray-900">Kuldeep Singh</h3>
+            <p class="text-sm font-semibold leading-6 text-indigo-600">Co-Founder / CEO</p>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="flex items-center gap-x-6">
+          <img class="h-16 w-16 rounded-full" src="https://images.unsplash.com/photo-1628155930542-3c7a64e2c833?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y2FydG9vbiUyMGZhY2V8ZW58MHx8MHx8fDA%3D" alt=""/>
+          <div>
+            <h3 class="text-base font-semibold leading-7 tracking-tight text-gray-900">Ankur Singh</h3>
+            <p class="text-sm font-semibold leading-6 text-indigo-600">Project Manager</p>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="flex items-center gap-x-6">
+          <img class="h-16 w-16 rounded-full" src="https://images.unsplash.com/photo-1583946099390-4ed248a602c0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzB8fGVtb2ppfGVufDB8fDB8fHww" alt=""/>
+          <div>
+            <h3 class="text-base font-semibold leading-7 tracking-tight text-gray-900">Nishant Gupta</h3>
+            <p class="text-sm font-semibold leading-6 text-indigo-600">Developer</p>
+          </div>
+        </div>
+      </li>
+
+
+    </ul>
+  </div>
+      </div>
+
     </>
   );
 }
